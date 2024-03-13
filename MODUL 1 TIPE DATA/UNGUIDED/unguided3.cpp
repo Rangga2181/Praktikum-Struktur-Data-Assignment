1d360f27cf89cd00e6f71e3536f7871a524ee9be
Rangga2181 <162523255+Rangga2181@users.noreply.github.com>
2024-03-13 16:48:21 +0700

    Update unguided3.cpp

--- MODUL 1 TIPE DATA/UNGUIDED/unguided3.cpp
+++ MODUL 1 TIPE DATA/UNGUIDED/unguided3.cpp
@@ -9,14 +9,14 @@ int main()
     map<string, int> umur;
 
     // Menambahkan data ke dalam map
-    umur["John"] = 25;
-    umur["Jane"] = 30;
-    umur["Doe"] = 20;
+    umur["Jawier"] = 25;
+    umur["Jandul"] = 30;
+    umur["Doel"] = 20;
 
     // Mengakses nilai dalam map
-    cout << "Umur John: " << umur["John"] << " tahun" << endl;
-    cout << "Umur Jane: " << umur["Jane"] << " tahun" << endl;
-    cout << "Umur Doe: " << umur["Doe"] << " tahun" << endl;
+    cout << "Umur Jawir: " << umur["Jawier"] << " tahun" << endl;
+    cout << "Umur Jandul: " << umur["Jandul"] << " tahun" << endl;
+    cout << "Umur Doel: " << umur["Doel"] << " tahun" << endl;
 
     // Menggunakan iterator untuk menampilkan semua pasangan kunci-nilai dalam map
     cout << "\nDaftar Nama dan Umur:\n";
